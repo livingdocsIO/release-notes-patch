05c224888719cff1dd9a3b47770ded08c46629cd
Ralph Meier <ralphmeier79@gmail.com>
2021-10-19 11:08:44 +0200

    fix: fix property return in getPullBySha

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-console.log('release-notes-patch -> v1.5.0 (add auto draft updates)')
+console.log('release-notes-patch -> v1.5.1 (add auto draft updates)')
 const argv = require('yargs')
   .demandOption(['token', 'owner', 'repo', 'sha', 'tag'])
   .help(false)
