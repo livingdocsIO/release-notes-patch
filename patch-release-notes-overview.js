d3ea6461a7d58540fd3b1ea566ccff0e1022d9c3
Marc Bachmann <marc.brookman@gmail.com>
2025-04-30 10:20:55 +0200

    fix: Use 2 space indentation in releases.json file

--- patch-release-notes-overview.js
+++ patch-release-notes-overview.js
@@ -20,7 +20,7 @@ module.exports = async ({token, repo, tag, release} = {}) => {
     console.log(`\n\nUpdate Release Notes overview: Set '${release.branchName}#serverVersion' to ${tag}\n\n`)
   }
 
-  const patchedBase64ReleaseNotesOverview = Buffer.from(JSON.stringify(patchedContent, null, 4)).toString('base64')
+  const patchedBase64ReleaseNotesOverview = Buffer.from(JSON.stringify(patchedContent, null, 2)).toString('base64')
 
   return {patchedBase64ReleaseNotesOverview, releaseNotesOverviewSha: releaseNote.sha, releaseNotesOverviewHtml: releaseNote.html_url}
 }
